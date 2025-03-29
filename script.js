3b638a99b89b7a403d7b920b269065430cc5807d
nitrorich123 <gd5948144@gmail.com>
2025-03-29 19:48:52 -0400

    Update and rename script.js to git rm filename

--- script.js
+++
@@ -1,8 +0,0 @@
-// Example of a simple script that can be used for future enhancements
-document.querySelector('form').addEventListener('submit', function(e) {
-  const email = document.querySelector('#email').value;
-  if (!email.includes('@')) {
-    alert('Please enter a valid email address');
-    e.preventDefault();
-  }
-});
